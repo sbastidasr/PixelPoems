6770aacdbb9ce2ff5063b1e9d05ebae1aff38f82
Sebastian Bastidas <sbastidasr@hotmail.com>
2015-02-05 10:56:22 -0500

    Dynamically create sized and gestureRecognized labels from array with words.

--- FridgeWords/ViewController.m
+++ FridgeWords/ViewController.m
@@ -21,95 +21,41 @@
 }
 
 
-
-
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    CGRect labelFrame = CGRectMake( 10, 40, 100, 30 );
-    UILabel* label = [[UILabel alloc] initWithFrame: labelFrame];
-    [label setText: @"My Label"];
-  //                                 x  y  width height
-    CGRect labelFrame2 = CGRectMake( 30, 40, 100, 30 );
-    UILabel* label1 = [[UILabel alloc] initWithFrame: labelFrame2];
-    [label1 setText: @"My Label1"];
-
+     NSMutableArray *arrayForLabels=   [NSMutableArray array];
+  
+   NSArray *palabras =[NSArray arrayWithObjects: @"She", @"is", @"a", @"goddess", @"to", @"me", @"my", @"sun", @"and", @"my", @"moon", @"luscious", @"garden", @"of", @"beauty", @"my", @"ship", @"through", @"the", @"storm", @"and", @"d", @"at", @"butt", @"hot", nil];
+    
+//  NSArray *palabras = [NSArray arrayWithObjects: @"wird", @"word", @"holi", @"sebi", nil];
     
     
-    NSMutableArray *arrayForLabels=   [NSMutableArray array];
-    [arrayForLabels addObject:label];
-    [arrayForLabels addObject:label1];
-    
-    // enable touch delivery
-    label.userInteractionEnabled = YES;
-    label1.userInteractionEnabled = YES;
-    
-    
-    for (UILabel *myLabel in arrayForLabels) {
+    for (int i=0; i<palabras.count; i++)
+    {
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100,40+(i*40), 100, 100)];
+        
+        label.textColor = [UIColor blackColor];
+        label.font = [UIFont systemFontOfSize:30];
+        label.backgroundColor=[UIColor yellowColor];
+       // titleLabel.numberOfLines = 1;
+        //titleLabel.lineBreakMode = UILineBreakModeWordWrap;
+        label.text = palabras[i];
+        label .userInteractionEnabled = YES;  //enable user interaction
         
         UIPanGestureRecognizer *gesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)] ;
         
-        [myLabel addGestureRecognizer:gesture];
+        [label addGestureRecognizer:gesture];
+        
+        [label sizeToFit];
+        NSLog(@"Label's frame is: %@", NSStringFromCGRect(label.frame));
+        
+            [arrayForLabels addObject:label];//add to arrayforlabels
+        [self.view addSubview:label]; // add to view
+        
+        
     }
-      [self.view addSubview: label];
-      [self.view addSubview: label1];
-    
-/*
-      UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
-[self.movableLabel addGestureRecognizer:panRecognizer];
-    
-
-    
-    CGRect labelFrame = CGRectMake( 10, 40, 100, 30 );
-    UILabel* label = [[UILabel alloc] initWithFrame: labelFrame];
-    [label setText: @"My Label"];
-    [label setTextColor: [UIColor orangeColor]];
-
-    
-    label.userInteractionEnabled = YES;
-    [label addGestureRecognizer:panRecognizer];
-    
-    
-    [self.view addSubview: label];
-    */
-    //recognizer
-  
-    
-    /*
-    
-    for (int i=0; i<10; i++)
-    {
-        //For printing the text using for loop we change the cordinates every time for example see the "y=(i*20) like this we change x also so every time when loop run,the statement will print in different location"
-        
-        //This is use for adding dynamic label and also the last line must be written in the same scope.
-        
-        UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(100,(i*40), 100, 100)];
-        
-        //titleLabel = titleLabel;
-        //[titleLabel release];
-        
-        titleLabel.textColor = [UIColor blackColor];
-        titleLabel.font = [UIFont italicSystemFontOfSize:20];
-        titleLabel.numberOfLines = 5;
-        titleLabel.lineBreakMode = UILineBreakModeWordWrap;
-        titleLabel.text = @"mihir patel";
-    
-        
-        //Calculate the expected size based on the font and linebreak mode of label
-        CGSize maximumLabelSize = CGSizeMake(300,400);
-        CGSize expectedLabelSize = [@"mihir" sizeWithFont:titleLabel.font constrainedToSize:maximumLabelSize lineBreakMode:titleLabel.lineBreakMode];
-        //Adjust the label the the new height
-        CGRect newFrame = titleLabel.frame;
-        newFrame.size.height = expectedLabelSize.height;
-        titleLabel.frame = newFrame;
-        [self.view addSubview:titleLabel];
-        
-        //recognizer
-        [self.view.subviews[0] addGestureRecognizer:panRecognizer];
-    }*/
 
     
 }
