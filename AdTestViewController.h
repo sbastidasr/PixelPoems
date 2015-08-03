c3c1f03df5e16f2a8bd56e34a29ed529b77d148b
Sebastian Bastidas <sbastidasr@hotmail.com>
2015-08-03 12:50:34 -0500

    Added iAd Test

---
+++ AdTestViewController.h
@@ -0,0 +1,14 @@
+//
+//  AdTestViewController.h
+//  PixelPoems
+//
+//  Created by Sebastian Bastidas on 8/3/15.
+//  Copyright (c) 2015 sbastidasr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
+
+@interface AdTestViewController : UIViewController <ADBannerViewDelegate>
+
+@end
---
+++ AdTestViewController.m
@@ -0,0 +1,75 @@
+//
+//  AdTestViewController.m
+//  PixelPoems
+//
+//  Created by Sebastian Bastidas on 8/3/15.
+//  Copyright (c) 2015 sbastidasr. All rights reserved.
+//
+
+#import "AdTestViewController.h"
+#import <iAd/iAd.h>
+
+@interface AdTestViewController ()
+{
+    BOOL _bannerIsVisible;
+    ADBannerView *_adBanner;
+}
+@end
+
+@implementation AdTestViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    _adBanner = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, 320, 50)];
+    _adBanner.delegate = self;
+}
+
+
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    if (!_bannerIsVisible)
+    {
+        // If banner isn't part of view hierarchy, add it
+        if (_adBanner.superview == nil)
+        {
+            [self.view addSubview:_adBanner];
+        }
+        
+        [UIView beginAnimations:@"animateAdBannerOn" context:NULL];
+        
+        // Assumes the banner view is just off the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, -banner.frame.size.height);
+        
+        [UIView commitAnimations];
+        
+        _bannerIsVisible = YES;
+    }
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"Failed to retrieve ad");
+    
+    if (_bannerIsVisible)
+    {
+        [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
+        
+        // Assumes the banner view is placed at the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
+        
+        [UIView commitAnimations];
+        
+        _bannerIsVisible = NO;
+    }
+}
+
+@end
